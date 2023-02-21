da30b259ed05ef15d127f16640e6af27231ccce2
Thanawat James Jaisert <89368503+JameSs-66@users.noreply.github.com>
2023-02-21 18:31:49 +0700

    เสร็จ

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,29 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect Rec1, Rect Rec2){
+	double xLength = 0,yLength = 0, start = 0, end = 0;
+	if(Rec1.x + Rec1.w <= Rec2.x || Rec2.x + Rec2.w <= Rec1.x) return 0;
+	if(Rec1.y - Rec1.h >= Rec2.y || Rec2.y - Rec2.h >= Rec1.y) return 0;
+	if(Rec1.x > Rec2.x) start = Rec1.x;
+	else start = Rec2.x;
+	if(Rec1.x + Rec1.w < Rec2.x + Rec2.w) end = Rec1.x + Rec1.w;
+	else end = Rec2.x + Rec2.w; 
+	xLength = start - end;
+	start = 0; 
+	end = 0;
+	if(Rec1.y < Rec2.y) start = Rec1.y;
+	else start = Rec2.y;
+	if(Rec1.y - Rec1.h > Rec2.y - Rec2.h) end = Rec1.y - Rec1.h;
+	else end = Rec2.y - Rec2.h; 
+	yLength = start - end;
+	return abs(xLength * yLength);
+}
 
-
+int main() {
+	
+Rect R1 = {-1,2,6.9,9.6};
+Rect R2 = {0,0,1.2,2.5};
+cout << overlap(R1,R2);	
 
 }
